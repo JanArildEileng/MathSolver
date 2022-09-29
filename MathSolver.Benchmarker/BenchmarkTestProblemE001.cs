e8e26ea8f73a967f097ec5c0961551b94181a221
Øistein Stemme <Oistein.Stemme@fhi.no>
2022-09-29 14:15:33 +0200

    Øisteins analytiske løsningsforslag

--- MathSolver.Benchmarker/BenchmarkTestProblemE001.cs
+++ MathSolver.Benchmarker/BenchmarkTestProblemE001.cs
@@ -1,4 +1,5 @@
 ﻿using MathSolver.Mysolution.Jaei;
+using MathSolver.Mysolution.Oist;
 
 namespace MathSolver.Benchmarker;
 
@@ -20,7 +21,12 @@ public  class BenchmarkTestProblemE001
     [Benchmark(Description ="jaei")]
     public int JaeiSolution()
     {
-      return  new E001Multiplesof3and5().Sum(below: 1000); 
+        return  new E001Multiplesof3and5().Sum(below: 1000);
     }
 
+    [Benchmark(Description = "Oist")]
+    public int OistSolution()
+    {
+        return new E001MultiplesOf3And5UsingArithemeticSeriesSum().Sum(below: 1000);
+    }
 }
---
+++ MathSolver.Test/Oist/ProblemE001Test.cs
@@ -0,0 +1,10 @@
+﻿namespace MathSolver.Test.Oist
+{
+    public class ProblemE001Test : ProblemE001TestBase
+    {
+        public ProblemE001Test()
+        {
+            sut = new Mysolution.Oist.E001MultiplesOf3And5UsingArithemeticSeriesSum();
+        }
+    }
+}
